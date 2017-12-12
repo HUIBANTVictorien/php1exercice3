17932e1c3457f3a1228bd8b56eb51efdd313c9e0
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 16:05:33 +0100

    maj exo3

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
-<?php
-$km = 1;
-echo $km.'<br/>';
-$km = 3;
-echo $km.'<br/>';
-$km = 125;
-echo $km.'<br/>';
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 3 partie 1 php</title>
+  </head>
+  <body>
+    <?php
+    $km = 1;
+    echo $km.'<br/>';
+    $km = 3;
+    echo $km.'<br/>';
+    $km = 125;
+    echo $km.'<br/>';
+    ?>
+  </body>
+</html>
